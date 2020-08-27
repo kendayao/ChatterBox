82c28997a9468c4c25af29609001c9a713cb9be1
Kaeneth Dayao <kendayao@gmail.com>
2020-08-27 13:00:25 -0700

    sigin styling

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,7 +2,7 @@ import React, {useState} from "react"
 import { Link, useLocation } from "react-router-dom";
 import {useDispatch} from 'react-redux';
 import {getName} from '../actions/index'
-
+import "./SignIn.css"
 
 function SignIn(){
     const dispatch=useDispatch();
@@ -16,18 +16,18 @@ function SignIn(){
     }
 
     return(
-        <div>
-            <h1><i className="fas fa-comment-alt"></i>Chatterbox</h1>
-            <form>
-                <label htmlFor="name">ScreenName</label>
-                <input name="name" className="name" id="name" type="text" placeholder="Enter ScreenName" onChange={handleInputChange}></input>
+        <div className="signin-section">
+            <h1 className="logo"><i className="fas fa-comment-alt"></i>Chatterbox</h1>
+            <form className="name-input">
+                <input name="name" className="name" id="name" type="text" placeholder="Enter screen name" onChange={handleInputChange}></input>
                 <br/>
-            </form>
-            <button onClick={()=>dispatch(getName(screenName))}>
+            
+            <button onClick={()=>dispatch(getName(screenName))} className="start-chatting-btn">
                 <Link to="/chatroom" className={location.pathname === "/chatroom" ? "nav-link active" : "nav-link"}>
                     Start Chatting
                 </Link>
             </button>
+            </form>
         </div>
     )
 }
