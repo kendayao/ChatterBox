be53d52cc612edfb7b6c86449b34aa1ee66ffd3b
Kaeneth Dayao <kendayao@gmail.com>
2020-08-31 23:32:34 -0700

    added message alert

--- client/src/pages/Chatroom.js
+++ client/src/pages/Chatroom.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react"
 import io from 'socket.io-client'
 import {useSelector} from 'react-redux';
 import "./Chatroom.css"
+import Alert from "../Alert"
+import {Link, useLocation} from "react-router-dom"
 
 let socket;
 
 function Chatroom(){
-
+    const location = useLocation();
     const currentTime=new Date().toLocaleTimeString();
     const chatName=useSelector(state=>state.screenName)
     const [users, setUsers]=useState([])
@@ -15,7 +17,9 @@ function Chatroom(){
     const [socketid, setSocketid]=useState("")
     const [chatRoomName, setChatRoomName]=useState("")
     const [userName, setUserName]=useState("")
-    const [avatar, setAvatar]=useState("")
+    const [alertState, setAlertState]=useState(null)
+    const [disconnectedUser, setDisconnectedUser]=useState("")
+
     useEffect(()=>{
         socket = io.connect() 
         socket.on('connect', ()=>{
@@ -35,13 +39,20 @@ function Chatroom(){
 
             setUserName(message.name)
             setChat(chat=>[...chat, message])
-            // setAlertState(true)
-            // setTimeout(()=>{
-            //   setAlertState(null);
-            // }, 5000);
+            setAlertState(true)
+            setTimeout(()=>{
+              setAlertState(null);
+            }, 5000);
           })
           
           socket.on("disconnected", id =>{
+              setDisconnectedUser(function(users){
+                  users.forEach(user=>{
+                      if(user.id===id){
+                          return user.username
+                      }
+                  })
+              })
               setUsers(users=>{
                   return users.filter(user=>user.id !==id);
               })
@@ -51,8 +62,8 @@ function Chatroom(){
     }, [])
 
  
-    
-    
+    console.log(users)
+    console.log(disconnectedUser)
     const handleInputChange=event=>{
         const username=chatName
         const value=event.target.value
@@ -70,13 +81,10 @@ function Chatroom(){
         event.preventDefault();
         setSocketid(event.target.name)
         setChatRoomName(event.target.value)
-        setAvatar("far fa-user-circle visitor-avatar")
        
     }
 
   
-
-
     const renderUsers=()=>{
         return users.map(function(user){
             return (<div key={user.id}>
@@ -114,10 +122,6 @@ function Chatroom(){
     return (
         <div>
         
-        
-
-
-        
         <div className="row ">
             <div className="col-md-3 column-left">
                 <div className="column-left-header"> 
@@ -125,12 +129,14 @@ function Chatroom(){
                 </div>
           
             <h5>Users ready to chat</h5>
+            {alertState ? <Alert username={userName} alert={alertState} />: null}
             <div>{renderUsers()}</div>
             </div>
 
-
             <div className="col-md-9 column-right">
-                <a>Logout</a><br/>
+            <Link to="/" id="logout-link" className={location.pathname === "/" ? "nav-link active" : "nav-link"} >
+                    Logout
+                </Link>
                 <i class="fas fa-user-circle fa-8x"></i>
                 <h1>Welcome, {chatName}!</h1>
                 <p>Click on a user to open a Chatterbox</p>
@@ -146,7 +152,7 @@ function Chatroom(){
 
         <div className="modal" tabIndex="-1" role="dialog" id="exampleModal">
             <div className="modal-dialog modal-lg">
-                <div className="modal-content chat-modal">
+                <div className="modal-content chat-modal modal-dialog-scrollable">
                     <div className="modal-header">
                         <h5 className="modal-title"><i class="far fa-user-circle visitor-avatar"></i>{chatRoomName}</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
